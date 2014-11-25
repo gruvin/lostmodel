5e170d7c10fcbb2645ce0c2949f48c28227c1d44
gruvin@gmail.com <gruvin@gmail.com@31e11654-76c3-d051-5a62-695e1e5ec6c0>
2014-11-25 04:41:34 +0000

    pulseInWidth time-out counter improvements

--- firmware/main.h
+++ firmware/main.h
@@ -4,31 +4,32 @@
 // CW dit length (arbitrary units)
 #define DITLENGTH 8
 
+// TODO: prototype version. Pins changed for production version ...
 #define SIG_INP   PIND
 #define SIG_PORT  PORTD
 #define SIG_DDR   DDRD
-#define SIG_BIT   3
+#define SIG_BIT   3 // D-
 
 #define PIEZO_PORT PORTD
 #define PIEZO_DDR  DDRD
-#define PIEZO_BIT  5
+#define PIEZO_BIT  5 // LED01
 
 #define LED1_PORT PORTC
 #define LED1_DDR  DDRC
-#define LED1_BIT  3
+#define LED1_BIT  3 // LED05 (not used in prototype)
 
 #define LED2_PORT PORTD
 #define LED2_DDR  DDRD
-#define LED2_BIT  4
+#define LED2_BIT  4 // LED02
 
 #define LED3_PORT PORTC
 #define LED3_DDR  DDRC
-#define LED3_BIT  5
+#define LED3_BIT  5 // LED03
 
 #define LED_ON(a) LED ## a ## _PORT |= (1<<LED ## a ## _BIT)
 #define LED_OFF(a) LED ## a ## _PORT &= ~(1<<LED ## a ## _BIT)
 
-#define MIDPOINT 2400
+#define MIDPOINT 1400
 
 #define true (-1)
 #define false 0
